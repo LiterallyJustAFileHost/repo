7062908f26067ddff54eccb41c4f90be2d2fc10a
Sabio <gamester.sad@gmail.com>
2026-08-22 07:24:34 +0100

    add lucide, fix bug, update header css

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import { MoveUpRight } from "lucide-react";
+
 export default function Home() {
   return (
     <div>
-      <header className="flex flex-row py-5 px-[15%] items-center">
+      <header className="flex flex-row py-12 px-[20%] items-center">
         <p>Logo</p>
-        <button className="ml-auto uppercase">Sign Up</button>
+        <button className="ml-auto flex flex-row gap-1 items-center"><MoveUpRight size={16} /> Sign Up</button>
       </header>
     </div>
   );
