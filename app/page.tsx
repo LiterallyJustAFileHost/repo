d8283ba2bfbdca171a28e097ba3e4e66696fbc4d
Sabio <gamester.sad@gmail.com>
2026-08-23 22:18:04 +0100

    update sections around; add new section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import { Globe, MoveUpRight } from "lucide-react";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <header className="flex flex-row pt-12 px-[20%] items-center">
-        <p>Logo</p>
+        <img/>
+        <p>LiterallyJustAFileHost</p>
         <button className="ml-auto flex flex-row gap-1 items-center main-button"><MoveUpRight size={16} /> Sign Up</button>
       </header>
       <main className="text-center flex flex-col gap-5 my-40">
@@ -16,29 +18,50 @@ export default function Home() {
         </div>
       </main>
       <hr className="mx-[5dvw] border-2 opacity-50"></hr>
-      <div className="text-center flex flex-col items-center gap-5 my-20">
-        <h2 className="text-5xl">Less worrying, more storing</h2>
-        <p>Literally Just A File Host offers <span className="text-accent">2.5x</span> more storage than competitors, and growing.</p>
+      <div className="flex flex-row gap-10 justify-center my-20">
+        <div className="text-left flex flex-col gap-2">
+          <h2 className="text-5xl">Less worrying, more storing</h2>
+          <p>Literally Just A File Host offers <span className="text-accent">12.5x</span> more storage than some competitors, and growing.</p>
+        </div>
         <div className="flex flex-col gap-2 w-[30dvw] mt-2">
-          <div className="bg-accent w-full text-left pl-2 flex flex-row">
-            <p>Us</p>
+          <div className="bg-accent w-full pl-2 flex flex-row" title="A higher storage limit will be implemented as we grow.">
+            <p>LiterallyJustAFileHost <span className="opacity-75">(Us)</span></p>
             <p className="ml-auto mr-2 opacity-75">25GB</p>
           </div>
-          <div className="bg-red-500 w-[80%] text-left pl-2 opacity-25 flex flex-row">
+          <div className="bg-red-800 w-[80%] pl-2 flex flex-row" title="MEGA offers 20% less than LiterallyJustAFileHost.">
             <p>MEGA</p>
             <p className="ml-auto mr-2 opacity-75">20GB</p>
           </div>
-          <div className="bg-blue-700 w-[40%] text-left pl-2 opacity-25 flex flex-row">
+          <div className="bg-blue-700 w-[40%] pl-2 flex flex-row" title="MediaFire offers 60% less than LiterallyJustAFileHost.">
             <p>MediaFire</p>
             <p className="ml-auto mr-2 opacity-75">10GB</p>
           </div>
-          <div className="bg-blue-600 w-[8%] text-left pl-[8%] opacity-25 flex flex-row">
+          <div className="bg-blue-600 w-[8%] pl-[8%] flex flex-row" title="DropBox offers 92% less than LiterallyJustAFileHost.">
             <p className="ml-1.5">DropBox</p>
             <p className="ml-2 opacity-75">2GB</p>
           </div>
         </div>
       </div>
       <hr className="mx-[5dvw] border-2 opacity-50"></hr>
+      <div className="text-center flex flex-col items-center gap-5 my-20">
+        <h2 className="text-5xl">100% free and open-sourced, forever</h2>
+        <p>
+          This storage hoster is completely free and open-sourced, <span className="text-accent">forever</span>.
+          What we do with your data is to host it, <span className="text-accent">not to sell it</span>.
+        </p>
+        <div className="flex flex-row gap-2">
+          <a href="https://github.com/LiterallyJustAFileHost/repo" target="_blank">
+            <button className="main-button flex flex-row gap-1.5 items-center">
+              <svg role="img" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" width={16} height={16} fill="white">
+                <title>GitHub</title>
+                <path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
+              </svg>
+              GitHub
+            </button>
+          </a>
+          <Link href="/pricing" prefetch={true}><button>&quot;Pricing&quot;</button></Link>
+        </div>
+      </div>
     </div>
   );
 }
