d8f6b0b11ca16113a91dc50014813cb49f38a648
Flux3tor <tusharsinha199@gmail.com>
2026-08-30 23:56:34 +0530

    feat: add Google auth

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -10,21 +10,35 @@ export default function AuthPage() {
     });
   };
 
+  const handleGoogleAuth = async () => {
+    await authClient.signIn.social({
+      provider: "google",
+      callbackURL: "/dashboard",
+    });
+  };
+
   return (
     <div className="flex h-dvh">
       <div className="h-[90dvh] w-full flex flex-row mx-[8dvw] my-auto bg-[rgba(255,255,255,0.07)] border-4 border-[rgba(255,255,255,0.12)] backdrop-blur-lg rounded-2xl">
         <div className="flex flex-col gap-3 w-fit h-full p-[3dvw] border-r-4 border-[rgba(255,255,255,0.12)]">
           <h1 className="text-4xl font-bold">SIGN UP</h1>
-          <p>Powered by LiterallyJustAFileHost Auth</p>
+          <p>Powered by LiterallyJustAFileHost</p>
         </div>
 
-        <div className="flex flex-col w-full p-[3dvw]">
+        <div className="flex flex-col gap-4 justify-center w-full p-[3dvw]">
           <button
             onClick={handleHackClubAuth}
             className="bg-[rgba(255,255,255,0.07)] border-4 border-[rgba(255,255,255,0.12)] mx-[10dvw] backdrop-blur-lg rounded-3xl!"
-          >
-            Continue with Hack Club Auth
+            >
+              Continue with Hack Club Auth
           </button>
+
+          <button
+            onClick={handleGoogleAuth}
+            className="bg-[rgba(255,255,255,0.07)] border-4 border-[rgba(255,255,255,0.12)] mx-[10dvw] backdrop-blur-lg rounded-3xl!"
+          >
+            Continue with Google
+        </button>
         </div>
       </div>
     </div>
