8adea5ffcba6e102706bbd2f4b85c4f8aa75b349
Sabio <gamester.sad@gmail.com>
2026-09-04 13:30:44 +0100

    add a global 404 page

---
+++ app/global-not-found.tsx
@@ -0,0 +1,32 @@
+import { Metadata } from "next";
+import { IBM_Plex_Sans } from "next/font/google";
+import "./globals.css";
+import { ArrowLeft } from "lucide-react";
+import Link from "next/link";
+
+const ibm_plex_sans = IBM_Plex_Sans({
+  variable: "--font-ibm-plex-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+  description: 'The page you were looking for no longer exists.',
+}
+
+export default function GlobalNotFound() {
+  return (
+    <html lang="en" className={`${ibm_plex_sans.variable} h-full antialiased`}>
+      <body className="min-h-full flex flex-col">
+        <div className="text-center flex flex-col h-dvh justify-center">
+          <div className="flex flex-col gap-3">
+            <h1 className="text-9xl font-black">404</h1>
+            <p>The page you were looking for no longer exists.</p>
+          </div>
+          {/* eslint-disable-next-line */}
+          <a href="/dashboard"><button className="main-button flex flex-row gap-1 mx-auto mt-8 font-bold items-center justify-center w-fit"><ArrowLeft/> Dashboard</button></a>
+        </div>
+      </body>
+    </html>
+  )
+}
